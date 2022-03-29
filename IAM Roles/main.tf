8cb5c063227d60c59c04ad67be359f19b0096de5
kynh-2344 <nguyen.hong.ky@sun-asterisk.com>
2022-03-29 08:49:36 +0700

    Use template to render policy

--- IAM Roles/main.tf
+++ IAM Roles/main.tf
@@ -7,7 +7,7 @@ resource "aws_iam_role" "iam_role_ec2" {
 resource "aws_iam_role_policy" "iam_role_policy_ec2" {
     name                    = "${var.env}-iam-role-policy"
     role                    = aws_iam_role.iam_role_ec2.id
-    policy                  = file("${path.module}/policies/iam-policy.json")
+    policy                  = data.template_file.policy_template.rendered
 }
 
 resource "aws_iam_instance_profile" "iam_profile" {
@@ -15,7 +15,30 @@ resource "aws_iam_instance_profile" "iam_profile" {
     role                    = aws_iam_role.iam_role_ec2.name
 }
 
+data "template_file" "policy_template" {
+    template                = file("./policies/iam-policy.json")
+    vars = {
+        bucket_arn          = "${aws_s3_bucket.s3_bucket.arn}"
+    }
+}
 
+resource "aws_s3_bucket" "s3_bucket" {
+    bucket                  = "terraform-my-bucket-lab"
+    force_destroy           = true
+    tags = {
+      Name                  = "Bucket"
+    }
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket                    = aws_s3_bucket.s3_bucket.id
+  acl                       = "private"
+}
+
+resource "aws_s3_object" "log_folder" {
+  bucket                    = "${aws_s3_bucket.s3_bucket.bucket}"
+  key                       = "http-log/"
+}
 
 data "aws_ami" "ubuntu" {
   most_recent = true
@@ -34,9 +57,9 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  iam_instance_profile = aws_iam_instance_profile.iam_profile.name
+  ami                   = data.aws_ami.ubuntu.id
+  instance_type         = "t2.micro"
+  iam_instance_profile  = aws_iam_instance_profile.iam_profile.name
   tags = {
     Name = "HelloWorld"
   }
